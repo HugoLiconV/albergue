69b7399adbe5bca1eba9270ebf8b6613e35b22bf
axel <axelcazares012@gmail.com>
2018-04-07 00:22:41 -0600

    detalles donaciones

---
+++ src/app/donations-details/donations-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { DONATIONS } from '../../mock-donation';
+
+@Component({
+  selector: 'app-donations-details',
+  templateUrl: './donations-details.component.html',
+  styleUrls: ['./donations-details.component.css']
+})
+export class DonationsDetailsComponent implements OnInit {
+  donations = DONATIONS;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
