11a3eae1b0d446d4d74e590bc613eca2eb437197
Hugo <hugo_val28@hotmail.com>
2018-04-15 02:01:55 -0600

    added docs files

--- src/app/main-module/details-section/details-section.component.ts
+++ src/app/main-module/details-section/details-section.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-details-section',
   templateUrl: './details-section.component.html',
   styleUrls: ['./details-section.component.css',
-  '../../../flexboxgrid.min.css']
+  '../../../flexboxgrid.css']
 })
 export class DetailsSectionComponent implements OnInit {
 
