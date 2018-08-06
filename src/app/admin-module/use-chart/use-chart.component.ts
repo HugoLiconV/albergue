3fa1d35111f52946a887f25f46f9b6765a1418b7
Hugo <hugo_val28@hotmail.com>
2018-08-06 11:28:40 -0600

    add chart component

---
+++ src/app/admin-module/use-chart/use-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
+
+@Component({
+  selector: 'app-use-chart',
+  templateUrl: './use-chart.component.html',
+  styleUrls: ['./use-chart.component.css']
+})
+export class UseChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
